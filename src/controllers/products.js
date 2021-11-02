3d514a20313bb8bfeb687084e2e27ad73fdf5a47
Cleidianaa <cleidianapassos@outlook.pt>
2021-11-02 11:42:11 +0000

    trabalhando no codigo

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,6 +1,7 @@
 const ProductsModel = require('../models/products')
 
 function get(req, res) {
+    const products = await ProductsModel.find()
 
     res.send({
         ok: true
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,11 +4,8 @@ const ProductController = require('../controllers/products')
 
 router.get('/products', ProductController.get)
 //router.post('/products', ProductController.post)
+//router.put('/products/:id', ProductController.put)
+//router.delete('/products/:id', ProductController.delete)
 
-// VERBOS HTTP (4 TIPOS)
-// GET - obter  dados
-// POST - enviar/receber dados
-// PUT - atualizar dados
-// DELETE - remover dados
 
 module.exports = router
